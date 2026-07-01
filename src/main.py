671f46181e532f614bd9b5e7ec37cf7046fb334e
Jerome Vargas <jerovargas@est.ecotec.edu.ec>
2026-07-01 11:30:06 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-hellop
+# src/main.py
+
+import os
+
+# obtener entrada
+valor = os.environ.get("INPUT_VAL")
+
+# mostrar resultado en consola
+if "GITHUB_OUTPUT" in os.environ :
+        with open(os.environ["GITHUB_OUTPUT"], "a") as f :
+            print("{0}= Hola {1}".format('result',valor), file=f)
